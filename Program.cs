c0ac9bbc977b1a8320a48ae808b9ab170f844015
Azaruddin8297 <ajarpopo.com>
2023-03-17 19:10:28 +0530

    Programes

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BasicProgrames2
+{
+    public class Programes2
+    {
+        public static void Main(string[] args)
+        {
+            //Fabonicc.call();
+            //PerfectNumber.call();
+            //  PrimeNumber.call();
+            // ReverseNumber.call();
+            // ClassandObject obj = new ClassandObject();
+            // Console.WriteLine(obj.Empname);
+            StopWatch.call();
+        }
+    }
+}
